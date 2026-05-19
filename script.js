380d2fe5cdc927c0bc9514093a96f51cb60d885c
Ryan Dilley <ryandilley54@outlook.com>
2026-05-19 13:49:04 -0500

    rename submit to check, shuffle question order on start

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ var questions = [
   { image: '', answer: 'Mycobacterium tuberculosis' }
 ];
 
+function shuffle(arr) {
+  for (var i = arr.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  return arr;
+}
+
+shuffle(questions);
+
 var current = 0;
 var score = 0;
 var total = 0;
